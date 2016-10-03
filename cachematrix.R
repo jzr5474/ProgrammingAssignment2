c131b2c8ebb83820d1c166cc3f7b13d721cc4c3d
jzr5474 <jzr5474@psu.edu>
2016-10-03 00:54:47 -0400

    Project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a Cache where cacheSolve can 
+## check if the matrix has already been solved
 
-## Write a short comment describing this function
+## Explained above, not sure how much I learned here since I
+## literally just changed the names of the variables and the 
+## functions that were performed on them
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## First checks if the matrix has already been solved. If 
+## false, calculates it and then caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
